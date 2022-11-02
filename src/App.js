4ffa3c047f3038b666044a4c2527b008be234826
ondrejvosmera <ondravosmera@seznam.cz>
2022-11-02 18:47:21 +0100

    generator_done

--- src/App.js
+++ src/App.js
@@ -7,17 +7,29 @@ const App = () => {
   const getQuote = () => {
     fetch("https://type.fit/api/quotes")
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+    let randomNum = Math.floor(Math.random() * data.length);
+    setQuotes(data[randomNum]);
+    });
   };
+
+
+  useEffect(() => {
+    getQuote();
+  },[]);
 
   return (
     <div classname="quote-container">
-      <div className="quote">quote_text</div>
-      <div className="author">author</div>
-      <div className="socials"></div>
-      <button className="btn" onClick={getQuote}>
-        New quote
-      </button>
+      <div className="quote">
+        <p>{quotes.text}</p>
+       </div>
+      <div className="author">
+        <p>{quotes.author}</p>
+      </div>
+      <div className="twitter">
+        <a href={`https://twitter.com/intent/tweet?text=${quotes.text}`} target='_blank' rel='noreferrer' className='btn'>Twitter</a>
+      </div>
+      <button className="btn" onClick={getQuote}>New quote</button>
     </div>
   );
 };
