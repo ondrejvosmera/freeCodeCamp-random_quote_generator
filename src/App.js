5d8e1d31ec76af2d8fe1b45851fd1d30f1fa0af5
ondrejvosmera <ondravosmera@seznam.cz>
2022-11-02 20:12:38 +0100

    color_changing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from "react";
-import "./index.css";
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 const App = () => {
   const [quotes, setQuotes] = useState("");
-
+  const colors = ['#122932', '#FFBF46', '#9B5094', '#0B7A75', '#ED254E', '#FF8811', '#004643', '#AFC97E', '#890620', '#171A21', '#1F271B'];
+  const textRef = useRef();
+  const bgRef = useRef();
+  let randomColor = colors[Math.floor(Math.random() * colors.length)]
   const getQuote = () => {
     fetch("https://type.fit/api/quotes")
     .then((res) => res.json())
@@ -18,18 +21,25 @@ const App = () => {
     getQuote();
   },[]);
 
+  useEffect(() => {
+    textRef.current.style.color = randomColor
+  }, [quotes]);
+
+  useEffect(() => {
+    bgRef.current.style.backgroundColor = randomColor
+  }, [quotes]);
+
+
   return (
-    <div classname="quote-container">
-      <div className="quote">
-        <p>{quotes.text}</p>
-       </div>
-      <div className="author">
-        <p>{quotes.author}</p>
+    <div className='App' ref={bgRef}>
+      <div classname="quote" ref={textRef}>
+          <p>{quotes.text}</p>
+          <p>{quotes.author}</p>
+          <div classname='btn-container'>
+            <a href={`https://twitter.com/intent/tweet?text=${quotes.text}`} target='_blank' rel='noreferrer' className='btn'>Twitter</a>
+            <button className="btn" onClick={getQuote}>New quote</button>
+          </div>
       </div>
-      <div className="twitter">
-        <a href={`https://twitter.com/intent/tweet?text=${quotes.text}`} target='_blank' rel='noreferrer' className='btn'>Twitter</a>
-      </div>
-      <button className="btn" onClick={getQuote}>New quote</button>
     </div>
   );
 };
